7084be2e114f299429c73ba75ccb407c5244fbc8
Haruroid <Haruroid@users.noreply.github.com>
2021-04-04 22:39:26 +0900

    fix stability

--- main.go
+++ main.go
@@ -31,12 +31,16 @@ func HandleRequest(clientConn net.Conn) {
 			}()
 			go func() { //client->host(compress)
 				zw, _ := flate.NewWriter(hostConn, flate.BestCompression)
-				buf := make([]byte, 100)
+				buf := make([]byte, 4096)
 				for {
 					n, err := clientConn.Read(buf)
-					if n == 0 && err == io.EOF {
-						_ = clientConn.Close()
-						break
+					if n == 0 {
+						if err == io.EOF {
+							_ = clientConn.Close()
+							break
+						}
+						time.Sleep(time.Millisecond * 10)
+						continue
 					}
 					zw.Write(buf[:n])
 					zw.Flush()
@@ -48,12 +52,16 @@ func HandleRequest(clientConn net.Conn) {
 		} else {
 			go func() { //host->client(compress)
 				zw, _ := flate.NewWriter(clientConn, flate.BestCompression)
-				buf := make([]byte, 100)
+				buf := make([]byte, 4096)
 				for {
 					n, err := hostConn.Read(buf)
-					if n == 0 && err == io.EOF {
-						_ = hostConn.Close()
-						break
+					if n == 0 {
+						if err == io.EOF {
+							_ = hostConn.Close()
+							break
+						}
+						time.Sleep(time.Millisecond * 10)
+						continue
 					}
 					zw.Write(buf[:n])
 					zw.Flush()
